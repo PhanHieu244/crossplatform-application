cd27eef845140b50f5f95be6e58611edd9ae6da7
PhanHieu244 <phanhieu2443@gmail.com>
2024-09-18 13:08:14 +0700

    update data source

--- crossplatform/src/main/java/vn/edu/hust/project/crossplatform/CrossPlatformApplication.java
+++ crossplatform/src/main/java/vn/edu/hust/project/crossplatform/CrossPlatformApplication.java
@@ -2,8 +2,9 @@ package vn.edu.hust.project.crossplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CrossPlatformApplication {
 
 	public static void main(String[] args) {
