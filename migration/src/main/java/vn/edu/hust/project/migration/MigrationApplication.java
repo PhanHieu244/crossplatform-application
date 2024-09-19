ce6e3312efe070817f308129c73a3aa53e6adba1
PhanHieu244 <phanhieu2443@gmail.com>
2024-09-19 20:17:07 +0700

    update migration

--- migration/src/main/java/vn/edu/hust/project/migration/MigrationApplication.java
+++ migration/src/main/java/vn/edu/hust/project/migration/MigrationApplication.java
@@ -2,6 +2,7 @@ package vn.edu.hust.project.migration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MigrationApplication {
@@ -9,5 +10,8 @@ public class MigrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MigrationApplication.class, args);
 	}
-
+	@Bean
+	public MigrationCompletedListener migrationCompletedListener() {
+		return new MigrationCompletedListener();
+	}
 }
